71ef9de95cdf9c813623a44b96fe50a40b8a6bd8
Lorian Aberdeen <lorian.aberdeen@gmail.com>
2024-09-27 13:57:58 -0400

    hw 0

--- Math2001/Homework/hw0.lean
+++ Math2001/Homework/hw0.lean
@@ -12,5 +12,11 @@ for clearer statements and any special instructions. -/
 
 
 @[autograded 5]
-theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
-  sorry
+theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 := by
+  calc
+    n^2 = n*n := by ring
+      _ ≥ 5*n := by rel[hn]
+      _ = 2*n + 3*n := by ring
+      _ ≥ 2*n + 3*5 := by rel[hn]
+      _ = (2*n + 11) + 4 := by ring
+      _ > 2*n + 11 := by extra
