9fc4bef004be4ab1074b7ef14e13f9ca19e26bb9
Lorian Aberdeen <lorian.aberdeen@gmail.com>
2024-09-27 14:13:00 -0400

    hw 1
    [wip]: hw 1 2024-09-27 14:13:00-04:00
    
    [wip]: hw 1 2024-09-27 14:23:22-04:00

--- Math2001/Homework/hw1.lean
+++ Math2001/Homework/hw1.lean
@@ -14,20 +14,45 @@ for clearer statements and any special instructions. -/
 
 @[autograded 5]
 theorem problem1 {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 := by
-  sorry
+  have h3 : q = 3 := by addarith[h2]
+  calc
+    p = p + 4*q - 4*q := by ring
+    _ = 1 - 4*3 := by rw[h1,h3]
+    _ = -11 := by numbers
 
 @[autograded 5]
 theorem problem2 {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 := by
-  sorry
+  have hn : b = 1 :=
+  calc
+    b = ((a+2*b)-(a-b))/3 := by ring
+    _ = (4 - 1) /3 := by rw[h1,h2]
+    _ = 1 := by numbers
+  calc
+    a = a - b + b := by ring
+    _ = 1 + 1 := by rw[h2,hn]
+    _ = 2 := by numbers
+
 
 @[autograded 5]
 theorem problem3 {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 := by
-  sorry
+  calc
+    x^3 - 8*x^2 + 2*x = (x-8)*x^2 + 2*x := by ring
+    _ ≥ (9-8)*x^2 + 2*x := by rel[hx]
+    _ = x*(x+2) := by ring
+    _ ≥ 9*(9+2) := by rel[hx]
+    _ ≥ 3 := by numbers
 
 @[autograded 5]
 theorem problem4 {x : ℚ} : x ^ 2 - 2 * x ≥ -1 := by
-  sorry
+  calc
+    x^2 - 2*x = (x-1)^2 - 1 := by ring
+    _ ≥ -1 := by extra
 
 @[autograded 5]
 theorem problem5 (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
-  sorry
+  have hp: 0 ≤ (b+a) := by addarith[h1]
+  have hm: 0 ≤ (b-a) := by addarith[h2]
+  calc
+    b^2 = b^2 - a^2 + a^2 := by ring
+    _ = (b-a)*(b+a) + a^2 := by ring
+    _ ≥ a^2 := by extra
